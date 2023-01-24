60e2c28483c8f785607f655667a992c623265c34
Dell <Dell@DESKTOP-TK7GSO1>
2023-01-24 14:58:17 -0300

    Dataannotations endereço

--- WorflowSystem/Models/Endereco.cs
+++ WorflowSystem/Models/Endereco.cs
@@ -9,7 +9,7 @@ namespace Worflow.Models
 
         }
 
-        public Endereco(string cep, string logadouro,string numero,string bairro,string cidade,string uf)
+        public Endereco(string cep, string logadouro, string numero, string bairro, string cidade, string uf)
         {
             CEP = cep;
             Logadouro = logadouro;
@@ -21,13 +21,14 @@ namespace Worflow.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "CEP é obrigatório")]
-        [Display(Name = "CEP", Prompt = "Digite o CEP")]       
-        [MinLength(8, ErrorMessage = "CEP necessita de 8 caracteres.")]       
+        [Display(Name = "CEP", Prompt = "Digite o CEP")]
+        [MinLength(8, ErrorMessage = "CEP necessita de 8 caracteres.")]
+        [MaxLength(8)]       
         public string CEP { get; set; }
 
         [Required(ErrorMessage = "Logadouro é obrigatório")]
         [Display(Name = "Rua/Av", Prompt = "Digite o Logadouro")]
-        [MinLength(5, ErrorMessage = "Logadouro necessita de 5 caracteres.")]     
+        [MinLength(5, ErrorMessage = "Logadouro necessita de 5 caracteres.")]
         public string Logadouro { get; set; }
 
         [Required(ErrorMessage = "Número é obrigatório")]
@@ -36,17 +37,19 @@ namespace Worflow.Models
 
         [Required(ErrorMessage = "Bairro é obrigatório")]
         [Display(Name = "Bairro", Prompt = "Digite o Bairro")]
-        [MinLength(3, ErrorMessage = "Bairro necessita de 3 caracteres.")]       
+        [MinLength(3, ErrorMessage = "Bairro necessita de 3 caracteres.")]
         public string Bairro { get; set; }
 
         [Required(ErrorMessage = "Cidade é obrigatória")]
         [Display(Name = "Cidade", Prompt = "Digite a Cidade")]
-        [MinLength(5, ErrorMessage = "Cidade necessita de 5 caracteres.")]      
+        [MinLength(5, ErrorMessage = "Cidade necessita de 5 caracteres.")]
         public string Cidade { get; set; }
 
         [Required(ErrorMessage = "UF é obrigatória")]
         [Display(Name = "UF", Prompt = "Digite a UF")]
-        [MinLength(2, ErrorMessage = "UF necessita de 2 caracteres.")]        
+        [MinLength(2, ErrorMessage = "UF necessita de 2 caracteres.")]
+        [MaxLength(2)]
+        [RegularExpression(@"[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$", ErrorMessage = "Use apenas caracteres alfabéticos.")]
         public string UF { get; set; }
     }
 }
