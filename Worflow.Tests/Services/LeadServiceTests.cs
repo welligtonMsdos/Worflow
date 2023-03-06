47ed3e39904134df0ddf52559d46f864c715a19d
Dell <Dell@DESKTOP-TK7GSO1>
2023-03-06 17:19:17 -0300

    Lead service - tests

---
+++ Worflow.Tests/Services/LeadServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Worflow.Repository;
+using Worflow.Services;
+
+namespace Worflow.Tests.Services
+{
+    public class LeadServiceTests
+    {
+        private LeadService _leadService;
+        public LeadServiceTests()
+        {
+            _leadService = new LeadService(new Mock<ILeadRepository>().Object);
+        }
+    }
+}
